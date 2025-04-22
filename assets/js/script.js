c9511cb0fc7f6fea872c70e041ec76ca3dace919
SeppeGG <seppe.gevaert@leerling.de-passer.be>
2025-04-22 10:43:10 +0200

    functie fetchVakken

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,14 @@ function init() {
     document.querySelector('#addButton').addEventListener('click', addVak);
     fetchVakken();
 }
+
+async function fetchVakken() {
+    try{
+        let response = await fetch("http://localhost:5688/vakken");
+        let vakken = await response.json();
+        displayVakken(vakken);
+    }
+    catch (err){
+        console.error("Er is een fout opgetreden",err);
+    }
+}
